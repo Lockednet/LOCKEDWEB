d8c6ad4df637d08bd57f95dd17bc595ebdef7b9d
COPIADOR <105566081+Lockednet@users.noreply.github.com>
2023-03-30 09:49:08 -0300

    Add files via upload

--- install/ubuinst.sh
+++ install/ubuinst.sh
@@ -332,13 +332,10 @@ dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
 echo -e "\E[44;1;37m    INSTALANDO PAINEL    \E[0m"
 echo ""
-echo -e "\033[1;37mPARA VCS QUE RECLAMA TANTO AGORA TEM KEY"
-echo -e "PAINEL" | figlet | boxes -d stone -p a0v0 | lolcat
-echo -e "\033[1;37mPARA INSTALAR COMPRE A KEY,, APENAS 5,00"
-echo -e "\033[1;37mCHAVE PIX\033[1;36m  fcb9b761-a374-4e36-a98b-b28f129e97c3"
-echo -e "\033[1;37mENVIAR O COMPROVANTE PARA ➡️\033[1;36m @LOCKED_SSH"
+echo -e "WEB SWIT SSH" | figlet | boxes -d stone -p a0v0 | lolcat
+echo -e "                              \033[1;31mBy @LOCKED_SSH\033[1;36m" | lolcat
 echo ""
-chave=$(curl -sSL "https://raw.githubusercontent.com/Lockeds/KEYPWEB/main/chave") &>/dev/null
+chave=$(curl -sSL "https://github.com/Lockednet/LOCKEDWEB/raw/main/install/chave") &>/dev/null
 
 read -p "DIGITE A CHAVE DE INSTALAÇÃO: " key
     
@@ -405,13 +402,13 @@ echo -e "                              \033[1;31mBy @LOCKED_SSH\033[1;36m" | lol
 echo ""
 echo -e "\033[1;32mPAINEL INSTALADO COM SUCESSO!" | lolcat
 echo ""
-echo -e "\033[1;36m SEU PAINEL:\033[1;37m http://$IP:81/admin\033[0m"
+echo -e "\033[1;36m SEU PAINEL:\033[1;37m http://$IP/admin\033[0m"
 echo -e "\033[1;36m USUÁRIO:\033[1;37m admin\033[0m"
 echo -e "\033[1;36m SENHA:\033[1;37m admin\033[0m"
 echo ""
-echo -e "\033[1;36m LOJA DE APPS:\033[1;37m http://$IP:81/apps\033[0m"
+echo -e "\033[1;36m LOJA DE APPS:\033[1;37m http://$IP/apps\033[0m"
 echo ""
-echo -e "\033[1;36m PHPMYADMIN:\033[1;37m http://$IP:81/phpmyadmin\033[0m"
+echo -e "\033[1;36m PHPMYADMIN:\033[1;37m http://$IP/phpmyadmin\033[0m"
 echo -e "\033[1;36m USUÁRIO:\033[1;37m root\033[0m"
 echo -e "\033[1;36m SENHA:\033[1;37m $pwdroot\033[0m"
 echo ""
